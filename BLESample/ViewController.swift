6d4fda5bebb39376bbd0f23f1b07cf7d0353414a
Shinya Yamamoto <yamashin1003@hotmail.co.jp>
2016-11-02 12:15:57 +0900

    read data

--- BLESample/ViewController.swift
+++ BLESample/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreBluetooth
 
-class ViewController: UIViewController, CBCentralManagerDelegate {
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate  {
     var centralManager: CBCentralManager!
     var peripheral: CBPeripheral!
     override func viewDidLoad() {
@@ -20,11 +20,11 @@ class ViewController: UIViewController, CBCentralManagerDelegate {
     }
     
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        print("state :\(central.state)")
+        print("state :\(central.state.rawValue)")
+        print("state :\(central)")
         switch(central.state) {
             case .poweredOn:
                 //スキャン処理
-                //self.centralManger.scanForPeripheralsWithServices(nil, option:nil)
                 self.centralManager.scanForPeripherals(withServices: nil, options: nil)
                 break
             default:
@@ -47,10 +47,51 @@ class ViewController: UIViewController, CBCentralManagerDelegate {
     
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         print("接続成功")
+        
+        //Service探索
+        peripheral.delegate = self
+        peripheral.discoverServices(nil)
     }
     
     func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
         print("接続失敗...")
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        
+        let services:[CBService]? = peripheral.services
+        print("\(services?.count)個のサービスを発見")
+        if let services = services {
+            for aService in services {
+                peripheral.discoverCharacteristics(nil, for:aService)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?){
+        let characteristics:[CBCharacteristic]? = service.characteristics
+        print("\(characteristics?.count)個のキャラクタリスティックを発見")
+        
+        if let characteristics = characteristics {
+            for aCharacteristic in characteristics {
+                if aCharacteristic.properties == CBCharacteristicProperties.read {
+                    peripheral.readValue(for: aCharacteristic)
+                }
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        print("***********************")
+        print("読み取り成功")
+        print("uuid :\(characteristic.service.uuid)")
+        print("uuid :\(characteristic.uuid)")
+        print("value:\(characteristic.value)")
+        
+        let uuid = CBUUID(string:characteristic.uuid.uuidString)
+        print(uuid)
+        print("***********************")
+        
     }
 }
 
