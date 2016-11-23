160c16d30bd750479e3cd20b051073c61d6ec0cc
Shinya Yamamoto <yamashin1003@hotmail.co.jp>
2016-11-23 18:03:37 +0900

    セントラル２つを試す。

---
+++ BLESample/CentralViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CentralViewController.swift
+//  BLESample
+//
+//  Created by Shinya Yamamoto on 2016/11/23.
+//  Copyright © 2016年 Shinya Yamamoto. All rights reserved.
+//
+
+import Foundation
--- BLESample/ViewController.swift
+++ BLESample/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 import CoreBluetooth
 
-class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate  {
+class ViewController: UIViewController,
+                      CBCentralManagerDelegate,
+                      CBPeripheralDelegate,
+                      UITableViewDelegate,
+                      UITableViewDataSource{
     var centralManager: CBCentralManager!
     var peripheral: CBPeripheral!
+    
+    @IBOutlet weak var disconnectButton: UIButton!
+    @IBOutlet weak var tableView:UITableView!
+    var readDataArray = [String]()
+    var deviceArray = [CBPeripheral]()
+    
+    enum displayType {
+        case device
+        case readValue
+    }
+    
+    var currentDisplayType = displayType.device
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+
+        self.disconnectButton.addTarget(self, action: #selector(self.tapDisconnetButton), for: .touchUpInside)
+        
         //centralManager初期化
         self.centralManager = CBCentralManager(delegate:self, queue:nil)
     }
@@ -38,11 +61,19 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
                         rssi RSSI: NSNumber) {
         print("検出したデバイス \(peripheral)")
         
-        //保持しておく
-        self.peripheral = peripheral
+        var shouldAppend = true
+        for device in deviceArray {
+            if(device == peripheral) {
+                shouldAppend = false
+            }
+        }
+        if shouldAppend {
+            self.deviceArray.append(peripheral)
+            self.tableView.reloadData()
+        }
         
-        //接続処理
-        self.centralManager.connect(self.peripheral, options:nil)
+        
+        
     }
     
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
@@ -84,15 +115,78 @@ class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDe
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
         print("***********************")
         print("読み取り成功")
+        currentDisplayType = .readValue
         print("uuid :\(characteristic.service.uuid)")
         print("uuid :\(characteristic.uuid)")
         print("value:\(characteristic.value)")
+        
+        if let data = characteristic.value {
+            print(String(data: data, encoding: .utf8))
+            if let str = String(data: data, encoding: .utf8) {
+                self.readDataArray.append(str)
+                self.tableView.reloadData()
+            }
+        }
         
         let uuid = CBUUID(string:characteristic.uuid.uuidString)
         print(uuid)
         print("***********************")
         
     }
+    
+    //接続解除
+    @IBAction func tapDisconnetButton(sender:Any) {
+        currentDisplayType = .device
+        self.tableView.reloadData()
+    }
+    
+    //MARK:- TableView関連
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        switch currentDisplayType{
+        case .device:
+            return deviceArray.count
+        case .readValue:
+            return readDataArray.count
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        
+        switch currentDisplayType{
+        case .device:
+            let peripheral = deviceArray[indexPath.row]
+            var str = "\(peripheral.identifier.uuidString)\n"
+            str = str.appending("\(peripheral.name)\n")
+            str = str.appending("\(peripheral.state.rawValue)\n")
+            cell.textLabel!.text = str
+            cell.textLabel?.numberOfLines = 0
+            cell.textLabel?.font = UIFont.systemFont(ofSize: 12)
+            
+        case .readValue:
+            cell.textLabel!.text = readDataArray[indexPath.row]
+            
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //保持しておく
+        self.peripheral = deviceArray[indexPath.row]
+        
+        //接続処理
+        self.centralManager.connect(self.peripheral, options:nil)
+        
+    }
+    
 }
 
 
